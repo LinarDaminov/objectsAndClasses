3b6bad0bb18f9aac8bc38ab4e5621d17db70cab4
LinarDaminov <linar.daminov@yandex.ru>
2022-10-04 17:28:26 +0500

    Готовое ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Mark", "Twen");
+        Book book1 = new Book("Tom Soyer", author1, 2020);
 
-
+        Author author2 = new Author("Lev", "Tolstoy");
+        Book book2 = new Book("Voina i Mir", author2,1930);
+        System.out.println("Книга: " + book1.getBookName() + " Автор:"
+                + book1.getAuthor().getFirstName() + " " + author1.getLastName() + " год издания: " + book1.getPublishYear());
+        System.out.println("Книга: " + book2.getBookName() + " Автор:"
+                + book2.getAuthor().getFirstName() + " " + author2.getLastName() + " год издания: " + book2.getPublishYear());
+        book1.setPublishYear(2022);
+        System.out.println("Переиздание " + book1.getPublishYear());
     }
 }
